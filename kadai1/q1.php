7bf6bb3b70fa919d9f91f446f12f1ded675aee28
303B-16\cre <a>
2023-08-29 19:03:17 +0900

    課題

--- kadai1/q1.php
+++ kadai1/q1.php
@@ -2,6 +2,23 @@
 ある変数 num が 10 より大きい場合、「num is greater than 10」と表示する PHP プログラムを作成してください。 -->
 
 <?php
+if($count >10)
+{echo"BIG\n";}
 
-// hoge
+// hogeo
 ?>
+<!--問題 2（初級）:
+二つの変数 num1 と num2 があります。num1 が num2 より大きい場合は「num1 is greater than num2」、小さい場合は「num1 is less than num2」、等しい場合は「num1 is equal to num2」と表示する PHP プログラムを作成してください。
+
+<?php
+if ($var > 10) {
+    echo "BIG\n";
+} elseif ($var== 10) {
+    echo "NORMAL\n";
+} else {
+    echo "SMALL\n";
+}
+?>
+
+問題 3（初級）:
+変数 num が奇数か偶数かを判定する PHP プログラムを作成してください。
