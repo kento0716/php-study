171f8434cb1a9df453ae87e619f9b2daa8405440
303B-16\cre <a>
2023-12-05 18:52:19 +0900

    kadai2

--- kadai2/kadai2問題8.php
+++ kadai2/kadai2問題8.php
@@ -1,7 +1,10 @@
 問題 8:
 $pets という名前の配列があり、その中にはいくつかのペットの名前が入っています。foreach ループを使用して、各ペットの名前の長さ（文字数）を表示する PHP プログラムを作成してください。
 
-
+<?php
+foreach ($pets as $pet) 
+$length = mb_strlen($pet);
+echo "{$pet}の名前の長さは{$
 
 
 
